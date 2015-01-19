d991e79101e016fb5f1833a4eb6fb85733bfe65d
Nicolas <nulysse4@gmail.com>
2015-01-18 20:32:10 -0500

    Can now search further results

--- eztorrent.py
+++ eztorrent.py
@@ -4,49 +4,98 @@ import os
 import transmissionrpc
 import base64
 
+#https://api.t411.me/
+
 class cmdLoop:
 
+    __result_len_limit__ = 20
+
     def __init__(self):
-        self.t411 = t411.T411()
-        self.transmission = transmissionrpc.Client(address='nicorasp.local', port=9091)
+
+        try:
+            print 'Connecting to T411'
+            self.t411 = t411.T411()
+        except Exception as e:
+            print 'Could not connect to T411: '+str(e)
+
+        try:
+            print 'Connecting to Transmission'
+            self.transmission = transmissionrpc.Client(address='nicorasp.local', port=9091)
+        except Exception as e:
+            print 'Could not connect to Transmission: '+str(e)
+
+        self.clear()
+
+    def clear(self):
+        self.offset = 0
+        self.last_search_result = dict()
+        self.last_query_string = ''
 
     def __main_menu(self):
 
         print 'Choose between'
         print '\t-\'s\'\tSearch'
         print '\t-\'d\'\tDownload'
+        print '\t-\'n\'\tNext'
         print '\t-\'q\'\tQuit'
         return raw_input('Prompt: ')
 
-    def search(self, argument):
+    def get_search_string(self, query):
 
-        self.last_search_result = self.t411.search(argument).json()
+        return query+'?offset='+str(self.offset)+'&limit='+str(self.__result_len_limit__)
 
-        for i, torrent in enumerate(self.last_search_result['torrents']):
-            print '\t-{!s} {}'.format(i, torrent['name'].encode('utf-8'))
+    def print_search_results(self):
+        if self.last_search_result:
+            for i, torrent in enumerate(self.last_search_result['torrents']):
+                print '\t-{!s} {}'.format(i, torrent['name'].encode('utf-8'))
+        else:
+            print 'Nothing found.'
 
-    def download(self, argument):
+    def search_t411(self):
+        return self.t411.search(self.get_search_string(self.last_query_string)).json()
+
+    def search(self, *args):
+
+        self.last_query_string = str(*args[0])
+        self.last_search_result = self.search_t411()
+
+        self.print_search_results()
+
+    def next(self, *args):
+        self.offset += self.__result_len_limit__
+
+        self.last_search_result = self.search_t411()
+
+        self.print_search_results()
+
+    def download(self, *args):
 
         if not os.path.exists(r'.\temp'):
             os.makedirs(r'.\temp')
 
-        torrent = self.t411.download(self.last_search_result['torrents'][int(argument)]['id'])
+        torrent = self.t411.download(self.last_search_result['torrents'][int(args[0])]['id'])
 
         self.transmission.add_torrent(base64.b64encode(torrent.content))
 
     def run(self):
 
         choice = ''
-        actions = {'s': self.search, 'd': self.download}
+        actions = {'s': self.search,
+                   'd': self.download,
+                   'n': self.next}
 
         while choice != 'q':
 
             choice = self.__main_menu()
 
             try:
-                actions[(choice.split())[0]]((choice.split())[1])
-            except Exception as e:
+                actions[(choice.split())[0]]((choice.split())[1:])
+            except KeyError as e:
                 print 'Command {!s} not recognized'.format(choice)
+            except Exception as e:
+                print 'Command {!s} failed -{!s}-'.format((choice.split())[1:],e)
+
+
 
 if __name__ == '__main__':
 
