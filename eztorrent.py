08bf8f88897b3fa9db1a7d53c9c4ec58a57a1bc0
Nicolas <nulysse4@gmail.com>
2015-01-21 19:04:06 -0500

    Playing around categories

--- eztorrent.py
+++ eztorrent.py
@@ -40,7 +40,8 @@ class cmdLoop:
         print '\t-\'p\'\tPrevious'
         print '\t-\'c\'\tClear'
         print '\t-\'i\'\tInfo'
-        print '\t-\'i\'\tUser'
+        print '\t-\'u\'\tUser'
+        print '\t-\'c\'\tCat'
         print '\t-\'q\'\tQuit'
         return raw_input('Prompt: ')
 
@@ -49,9 +50,14 @@ class cmdLoop:
         return query+'?offset='+str(self.offset)+'&limit='+str(self.__result_len_limit__)
 
     def print_search_results(self):
+
+        print 'Found {!s} torrent'.format(self.last_search_result['total'])
         if self.last_search_result:
             for i, torrent in enumerate(self.last_search_result['torrents']):
-                print '\t-{!s} {}'.format(i, torrent['name'].encode('utf-8'))
+                print '\t-{!s} {} [{!s} -{!s}-]'.format(i,
+                                                        torrent['name'].encode('utf-8'),
+                                                        torrent['categoryname'].encode('utf-8'),
+                                                        torrent['category'])
         else:
             print 'Nothing found.'
 
@@ -102,6 +108,22 @@ class cmdLoop:
         else:
             print 'You need to make a search first.'
 
+    def cat(self, cmdArgs, filters):
+
+        cat_list = self.t411.categories().json()
+        if cmdArgs:
+            print cat_list
+        else:
+            for cat_id, cat_info in cat_list.iteritems():
+                if 'id' in cat_info:
+
+                    print '\t-{!s}:\t{!s}'.format(cat_id, cat_info['name'].encode('utf-8'))
+
+                    if 'cats' in cat_info:
+                        for subcat_id, subcat_info in cat_info['cats'].iteritems():
+                            print '\t\t-{!s}:\t{!s}'.format(subcat_id, subcat_info['name'].encode('utf-8'))
+
+
     def download(self, cmdArgs, filters):
 
         torrent = self.t411.download(self.last_search_result['torrents'][int(cmdArgs)]['id'])
@@ -112,14 +134,17 @@ class cmdLoop:
     def parse_command_line(line):
 
         filters = list()
+        cmdArgs = ''
+        cmd = ''
 
         for i, ele in enumerate(line.split('|')):
             if i:
-                print i
                 filters.append({'type':(ele.split())[0], 'arg':(ele.split())[1:]})
             else:
                 cmd = (ele.split())[0]
-                cmdArgs = (ele.split())[1:][0]
+
+                if len(ele.split())>1:
+                    cmdArgs = (ele.split())[1:][0]
 
         return cmd, cmdArgs, filters
 
@@ -132,7 +157,8 @@ class cmdLoop:
                    'p': self.previous,
                    'c': self.clear,
                    'i': self.info,
-                   'u': self.user}
+                   'u': self.user,
+                   'c': self.cat}
 
         while cmd != 'q':
 
@@ -141,7 +167,7 @@ class cmdLoop:
             try:
                 actions[cmd](cmdArgs, filters)
             except KeyError as e:
-                print 'Command {!s} not recognized'.format(cmd)
+                print 'Command {!s} not recognized-{!s}-'.format(cmd, e)
             except Exception as e:
                 print 'Command {!s} failed -{!s}-'.format((cmd.split())[1:], e)
 
