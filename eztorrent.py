de6aa7d3c18706a017ac2b8ba7a7fb0647d9462e
Nicolas <nulysse4@gmail.com>
2015-01-25 20:18:11 -0500

    Getting there!

--- eztorrent.py
+++ eztorrent.py
@@ -21,7 +21,7 @@ class cmdLoop:
     def __init__(self):
         self.__load_commands()
         self.__create_menu()
-        return
+
         try:
             print 'Connecting to T411'
             self.t411 = t411.T411()
@@ -36,44 +36,32 @@ class cmdLoop:
 
         self.clear()
 
-    def __load_commands(self):
-        self.commands = [{'cmd': method[1].command_string, 'method': method[1], 'doc': inspect.getdoc(method[1])}
-                         for method in inspect.getmembers(self, predicate=inspect.ismethod) if hasattr(method[1], 'command_string')]
+        print 'Type \'help\' for help'
 
-        print self.commands
+    def __load_commands(self):
+        self.commands = {method[1].command_string:  {'method': method[1], 'doc': inspect.getdoc(method[1])}
+                         for method in inspect.getmembers(self, predicate=inspect.ismethod)
+                         if hasattr(method[1], 'command_string')}
 
     def __create_menu(self):
 
         self.menu = 'T411:\n'
 
-        for cmd in self.commands:
-            doclines = cmd['doc'].splitlines()
-            self.menu += '\t-{!s}:\t{!s}\n'.format(cmd['cmd'], doclines[0])
+        for str, cmd in self.commands.iteritems():
 
-            if len(doclines)>1:
-                for line in doclines[1:]:
-                    self.menu += '\t\t{!s}\n'.format(line)
+            try:
+                doclines = cmd['doc'].splitlines()
+                self.menu += '\t-{!s}:\t\t{!s}\n'.format(str, doclines[0])
 
-        print self.menu
-
-    def clear(self, *args):
-        self.offset = 0
-        self.last_search_result = dict()
-        self.last_query_string = ''
+                if len(doclines)>1:
+                    for line in doclines[1:]:
+                        self.menu += '\t\t\th{!s}\n'.format(line)
+            except:
+                self.menu += '\t-{!s}\n'.format(str)
 
     def __main_menu(self):
 
-        print 'Choose between'
-        print '\t-\'s\'\tSearch'
-        print '\t-\'d\'\tDownload'
-        print '\t-\'n\'\tNext'
-        print '\t-\'p\'\tPrevious'
-        print '\t-\'c\'\tClear'
-        print '\t-\'i\'\tInfo'
-        print '\t-\'u\'\tUser'
-        print '\t-\'c\'\tCat'
-        print '\t-\'q\'\tQuit'
-        return raw_input('Prompt: ')
+        return raw_input('T411: ')
 
     def get_search_string(self, query, filters):
 
@@ -104,32 +92,48 @@ class cmdLoop:
     def search_t411(self, filters):
         return self.t411.search(self.get_search_string(self.last_query_string, filters)).json()
 
+    @command('clear')
+    def clear(self, *args):
+        """
+        Clear previous results
+        """
+        self.offset = 0
+        self.last_search_result = dict()
+        self.last_query_string = ''
 
-    @command('s')
+
+    @command('search')
     def search(self, cmdArgs, filters):
         """
-            Search Torrent
-            yrst
+            [query string] -> Search Torrent
+            accept filters:
+                | cuid category_id
         """
-
-
         self.last_query_string = str(cmdArgs)
         self.last_search_result = self.search_t411(filters)
 
         self.print_search_results()
 
-    @command('i')
+    @command('help')
+    def help(self, cmdArgs, filters):
+        print self.menu
+
+    @command('info')
     def info(self, cmdArgs, filters):
         """
-            Get Torrent Info
+            [torrentID] -> Get Torrent Info
         """
 
-        infos =  self.t411.details(self.last_search_result['torrents'][int(cmdArgs)]['id']).json()
+        infos = self.t411.details(self.last_search_result['torrents'][int(cmdArgs)]['id']).json()
 
         for key, value in infos['terms'].iteritems():
             print '\t- ' + key + ':\t' + value
 
+    @command('user')
     def user(self, cmdArgs, filters):
+        """
+        Show user data (ratio...)
+        """
 
         infos = self.t411.me().json()
 
@@ -137,7 +141,11 @@ class cmdLoop:
         print 'Downloaded:\t'+str(infos['downloaded'])+' bytes'
         print 'Ratio:\t'+str(float(infos['uploaded'])/float(infos['downloaded']))
 
+    @command('next')
     def next(self, cmdArgs, filterss):
+        """
+            Shows next results for last query
+        """
         if self.last_search_result:
             self.offset += self.__result_len_limit__
 
@@ -147,7 +155,11 @@ class cmdLoop:
         else:
             print 'You need to make a search first.'
 
+    @command('previous')
     def previous(self, cmdArgs, filters):
+        """
+            Shows previous results for last query
+        """
         if self.last_search_result:
             self.offset -= self.__result_len_limit__
 
@@ -159,8 +171,11 @@ class cmdLoop:
         else:
             print 'You need to make a search first.'
 
+    @command('cat')
     def cat(self, cmdArgs, filters):
-
+        """
+            List categories
+        """
         cat_list = self.t411.categories().json()
 
         for cat_id, cat_info in cat_list.iteritems():
@@ -172,8 +187,11 @@ class cmdLoop:
                     for subcat_id, subcat_info in cat_info['cats'].iteritems():
                         print '\t\t-{!s}:\t{!s}'.format(subcat_id, subcat_info['name'].encode('utf-8'))
 
-
+    @command('download')
     def download(self, cmdArgs, filters):
+        """
+            [search result index] -> Download torrent
+        """
 
         torrent = self.t411.download(self.last_search_result['torrents'][int(cmdArgs)]['id'])
 
@@ -200,21 +218,13 @@ class cmdLoop:
     def run(self):
 
         cmd = ''
-        actions = {'s': self.search,
-                   'd': self.download,
-                   'n': self.next,
-                   'p': self.previous,
-                   'c': self.clear,
-                   'i': self.info,
-                   'u': self.user,
-                   'c': self.cat}
 
         while cmd != 'q':
 
             cmd, cmdArgs, filters = self.parse_command_line(self.__main_menu())
 
             try:
-                actions[cmd](cmdArgs, filters)
+                self.commands[cmd]['method'](cmdArgs, filters)
             except KeyError as e:
                 print 'Command {!s} not recognized-{!s}-'.format(cmd, e)
             except Exception as e:
